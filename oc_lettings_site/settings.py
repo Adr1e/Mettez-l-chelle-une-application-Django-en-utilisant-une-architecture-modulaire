fad1b4a45bd3892c19c735b0bae0d8f2d0bf235d
Adr1e <adrienfischer274@gmail.com>
2025-12-16 19:29:46 +0100

    Fix: add Render domain to ALLOWED_HOSTS

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'oc-lettings-h475.onrender.com', '.onrender.com']
 
 # Sentry configuration
 sentry_sdk.init(
